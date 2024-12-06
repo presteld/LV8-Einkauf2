45ae7328db7e46911aa7a0419b700ecef5b39102
presteld <dietmar.prestel@hs-kempten.de>
2024-12-06 16:32:58 +0100

    3.Version Design des Bestellfensters fertig

--- src/Einkauf.java
+++ src/Einkauf.java
@@ -5,8 +5,15 @@ public class Einkauf extends JFrame {
 
 
     private JPanel jpPanel;
-
-
+    private JLabel lblStueckzahl;
+    private JLabel lblStueckpreis;
+    private JLabel lblRabatt;
+    private JTextField txtStueckzahl;
+    private JTextField txtStueckpreis;
+    private JTextField txtRabatt;
+    private JButton btnBerechnen;
+    private JTextArea taBestellung;
+    private JButton btnEnde;
 
 
     public Einkauf() throws HeadlessException {
